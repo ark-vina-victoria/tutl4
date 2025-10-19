f49fb43fb92fccf0b418e8228061a966087a2794
ark-vina-victoria <gubajin336@gmail.com>
2025-10-19 16:56:29 +0800

    test

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -10,5 +10,10 @@ public class CalcTest {
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
+        // 期望 4-2=2，但若 subtract 方法返回错误值（如 0、5），则测试失败
 
+    @Test
+    void testSubtraction() {
+        assertEquals(2, c.subtract(4,2));
+    }
 }
